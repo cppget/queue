0d3c5b1ecbd1b47ab1ca61ee5ed0232d56a3f2b3
Boris Kolpackov <boris@codesynthesis.com>
2018-08-21 14:04:02 +0200

    Add submission handler configuration

---
+++ submit.config.bash
@@ -0,0 +1,12 @@
+# The brep-submit-git submission handler configuration.
+
+# Note that submissions to the stable section are placed into testing (see
+# README for details).
+#
+sections[alpha]=1/alpha
+sections[beta]=1/beta
+sections[stable]=1/testing
+
+# Project/package name ownership information is in the owners/ subdirectory.
+#
+owners=owners
